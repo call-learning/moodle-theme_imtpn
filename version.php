a7e25b1badf99076db1635581c05a43dc9efaab7
Laurent David <lmedavid@gmail.com>
2023-11-09 14:30:16 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023092800; /* This is the version number to increment when changes needing an update are made */
+$plugin->version   = 2023092801; /* This is the version number to increment when changes needing an update are made */
 $plugin->requires  = 2022112500;
-$plugin->release   = '4.1.1';
+$plugin->release   = '4.1.2';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_imtpn';
 $plugin->dependencies = [
